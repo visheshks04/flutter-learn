ab89fca4e33b463e63af688447b6c90d90daa272
Vishesh Kumar Singh <visheshkrsinghofficial@gmail.com>
2022-03-27 00:11:19 +0530

    asset and network image

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('App Title comes here'),
-          centerTitle: true,
-          backgroundColor: Colors.orange,
-        ),
-        body: Center(
-          child: Text(
-            'The body comes here',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              textBaseline: TextBaseline.ideographic,
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('+'),
-          backgroundColor: Colors.orange,
-        ),
-      ),
+      home: Home(),
     ));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('App Title comes here'),
+        centerTitle: true,
+        backgroundColor: Colors.orange,
+      ),
+      body: Image.network(
+          'https://images.unsplash.com/photo-1473773508845-188df298d2d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('+'),
+        backgroundColor: Colors.orange,
+      ),
+    );
+  }
+}
